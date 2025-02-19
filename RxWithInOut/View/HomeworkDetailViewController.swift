ff6a18cf78484817f9b7dca1b2f996f729e5d8eb
Woohyun Jo <bluewing1004@gmail.com>
2025-02-19 19:09:17 +0900

    [Setting] 프로젝트 기본세팅

--- RxWithInOut/View/HomeworkDetailViewController.swift
+++ RxWithInOut/View/HomeworkDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeworkDetailViewController.swift
 //  RxWithInOut
 //
 //  Created by 조우현 on 2/19/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeworkDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
